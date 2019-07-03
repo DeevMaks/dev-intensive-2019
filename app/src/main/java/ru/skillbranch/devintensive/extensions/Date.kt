0f785a924c7af046a2bc6f90a251b20ff26b8853
Deev Maksim <deev.maks2012@ya.ru>
2019-07-04 00:15:08 +0500

    humanizeDiff

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -14,6 +14,16 @@ const val WEEK = DAY * 7
 const val MONTH = DAY * 30
 const val YEAR = DAY * 365
 
+val humanize = mapOf<TimeUnits, List<String>>(
+        TimeUnits.SECOND to listOf<String>("секуда", "секунды", "секунд"),
+        TimeUnits.MINUTE to listOf<String>("минута", "минуты", "минут"),
+        TimeUnits.HOUR to listOf<String>("час", "часа", "часов"),
+        TimeUnits.DAY to listOf<String>("день", "дня", "дней"),
+        TimeUnits.WEEK to listOf<String>("неделю", "недели", "недель"),
+        TimeUnits.MONTH to listOf<String>("месяц", "месяца", "месяцев"),
+        TimeUnits.YEAR to listOf<String>("год", "года", "лет")
+)
+
 fun Date.format(pattern:String="HH:mm:ss dd.MM.yy") : String {
     val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
     return dateFormat.format(this)
@@ -27,12 +37,33 @@ fun Date.add(value:Int, units:TimeUnits=TimeUnits.SECOND) : Date {
         TimeUnits.MINUTE -> value * MINUTE
         TimeUnits.HOUR -> value * HOUR
         TimeUnits.DAY -> value * DAY
+        TimeUnits.WEEK -> value * WEEK
+        TimeUnits.MONTH -> value * MONTH
+        TimeUnits.YEAR -> value * YEAR
     }
     this.time = time
     return this
 }
 
-fun Date.humanizeDiff(date:Date) : String {
+fun getHumanTime(count:Int, unit:TimeUnits) : String? = when (count) {
+    1, 21, 31, 41, 51 -> "$count ${humanize?.get(unit)?.get(0)}"
+    in 2..4, in 22..24, in 32..34, in 42..44, in 52..54 -> "$count ${humanize?.get(unit)?.get(1)}"
+    else -> "$count ${humanize?.get(unit)?.get(2)}"
+}
+
+fun getHumanDirection(dir:String, humanTime:String?) = if(dir == "future") "через $humanTime" else "$humanTime назад"
+
+fun Date.humanizeDiff(date:Date) : String? {
+    val timeDiff = abs(this.time - date.time)
+    val direction = if(this.time > date.time) "past" else "future"
+    when {
+        timeDiff > YEAR -> "${getHumanDirection(direction, getHumanTime((timeDiff / YEAR).toInt(), TimeUnits.YEAR))}"
+        timeDiff > MONTH -> "${getHumanDirection(direction, getHumanTime((timeDiff / MONTH).toInt(), TimeUnits.MONTH))}"
+        timeDiff > DAY -> "${getHumanDirection(direction, getHumanTime((timeDiff / DAY).toInt(), TimeUnits.DAY))}"
+        timeDiff > HOUR -> "${getHumanDirection(direction, getHumanTime((timeDiff / HOUR).toInt(), TimeUnits.HOUR))}"
+        timeDiff > MINUTE -> "${getHumanDirection(direction, getHumanTime((timeDiff / MINUTE).toInt(), TimeUnits.MINUTE))}"
+        else -> "несколько секунд назад"
+    }
     TODO()
 }
 
@@ -40,5 +71,8 @@ enum class TimeUnits{
     SECOND,
     MINUTE,
     HOUR,
-    DAY
+    DAY,
+    WEEK,
+    MONTH,
+    YEAR
 }
